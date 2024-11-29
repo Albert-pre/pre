f5fcd3c525756ec80d15ea518186837b943bdd9f
Albert-pre <cmpremunia@gmail.com>
2024-11-29 10:02:09 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '<generated_password>',
+        'HOST': '<generated_host>',
+        'PORT': '<generated_port>',
+    }
+}
